8de951b5097ef226e8fb5f01b9fe280c3dc228d3
Lr2k <Lr2k.Coding@gmail.com>
2022-06-05 12:03:20 +0900

    Update example.py

--- example.py
+++ example.py
@@ -8,16 +8,19 @@ choices = [
     "大神ミオ",
     "白上フブキ"
 ]
-
 message = "推しを選択してください。"
-
-menu = SelectMenu(choices, cursor=">", message=message)
-
-index = menu.start()
+menu = SelectMenu(choices, cursor=">", message=message, return_kinds='title')
 
 reconfirm_choices = ["Yes", "No"]
-reconfirm_message = choices[index] + "でよろしいですか？"
+reconfirm = SelectMenu(choices=reconfirm_choices, side_by_side=True)
 
-reconfirm = SelectMenu(choices=reconfirm_choices, message=reconfirm_message, side_by_side=True)
+while True:
+    title = menu.start()
+    reconfirm_message = title + "でよろしいですか？"
 
-print(reconfirm.start())
+    # menuに戻ってきた際に必ずカーソルの位置を一番上の選択肢に置きたい場合はstart(position=0)
+    # 前回選択した位置を引き継がせたい場合は、引数を指定しない
+    if reconfirm.start(message=reconfirm_message, position=1)==0:
+        break
+
+print(title)
